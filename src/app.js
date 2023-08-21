682f8f72ff9c48369c5e8e8894108574329887ba
travellerCoder <guillermobukowski@gmail.com>
2023-08-21 13:54:00 -0300

    Hola Mundo!

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'))
+
 app.listen (port, () =>
     console.log('**** Server Running ****')
+);
+
+app.get('/', (req, res) => 
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
 )
