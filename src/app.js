640607ebfc6fc1cc713765190fcd2a379191316d
travellerCoder <guillermobukowski@gmail.com>
2023-08-28 12:06:26 -0300

    instalacion de ejs y configuracion en entry point

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.set('view engine', 'ejs')
+
 const port = process.env.PORT || 3001;
 
 app.use(express.static('public'))
