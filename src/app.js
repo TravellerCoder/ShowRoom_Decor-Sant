f868a0d6b97cdd5220d6afb380a3867dfcc80aa0
travellerCoder <guillermobukowski@gmail.com>
2023-08-30 20:51:25 -0300

    se implemeta arquitectura MVC

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const methodOverride =  require('method-override');
 const path = require('path');
 const app = express();
+const mainRouter = require('./routers/mainRouter');
+
 
 app.set('view engine', 'ejs')
 
-const port = process.env.PORT || 3001;
 
+app.use(methodOverride("_method"))
 app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
+
+const port = process.env.PORT || 3001;
 app.listen (port, () =>
     console.log('**** Server Running ****')
 );
 
-app.get('/', (req, res) => 
-    res.render(path.resolve(__dirname, './views/index'))
-)
+
+
+app.use(mainRouter);
