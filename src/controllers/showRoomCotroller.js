9f0d6c827bf80087be7189c009246dae9f542d9e
travellerCoder <guillermobukowski@gmail.com>
2023-09-07 19:16:11 -0300

    creacion de vistas individuales de showRoom

--- src/controllers/showRoomCotroller.js
+++ src/controllers/showRoomCotroller.js
@@ -4,7 +4,35 @@ const fs = require('fs');
 const controller = {
     renderShowRoom: (req, res) =>{
         return res.render(path.resolve('src/views/showRoom'))
-    }
+    },
+
+    renderPisoFlotante: (req, res) => {
+        return res.render(path.resolve('src/views/showRoom/pisoFlotante'))
+    },
+
+    renderCortinas: (req, res) => {
+        return res.render(path.resolve('src/views/showRoom/cortinas'))
+    },
+
+    renderCesped: (req, res) => {
+        return res.render(path.resolve('src/views/showRoom/cesped'))
+    },
+
+    renderDeck: (req, res) => {
+        return res.render(path.resolve('src/views/showRoom/deck'))
+    },
+
+    renderPulido: (req, res) => {
+        return res.render(path.resolve('src/views/showRoom/pulido'))
+    },
+
+    renderRevestimiento: (req, res) => {
+        return res.render(path.resolve('src/views/showRoom/revestimiento'))
+    },
+
+    renderAlfombras: (req, res) => {
+        return res.render(path.resolve('src/views/showRoom/alfombra'))
+    },
 }
 
 module.exports = {controller}
--- src/routers/showRoomRouter.js
+++ src/routers/showRoomRouter.js
@@ -6,5 +6,19 @@ const showRoomController = require('../controllers/showRoomCotroller')
 
 router.get('/showRoom', showRoomController.controller.renderShowRoom)
 
+router.get('/showRoom/piso-flotante' , showRoomController.controller.renderPisoFlotante)
+
+router.get('/showRoom/cortinas' , showRoomController.controller.renderCortinas)
+
+router.get('/showRoom/cesped' , showRoomController.controller.renderCesped)
+
+router.get('/showRoom/deck' , showRoomController.controller.renderDeck)
+
+router.get('/showRoom/pulido' , showRoomController.controller.renderPulido)
+
+router.get('/showRoom/revestimiento' , showRoomController.controller.renderRevestimiento)
+
+router.get('/showRoom/alfombra' , showRoomController.controller.renderAlfombras)
+
 
 module.exports = router
